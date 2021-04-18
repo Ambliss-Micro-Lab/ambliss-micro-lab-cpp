9be16daec07ea8995e46df97f26fc1a8e18163ce
PHANINDRA05 <kakaraparthiphani0512@gmail.com>
2021-04-18 13:22:10 +0530

    added Demux encoder decoder

--- include/digital/Combinational.h
+++ include/digital/Combinational.h
@@ -13,8 +13,22 @@ class Combinational: public Gates{
         void full_adder(int x, int y, int cin, int* sum, int* carry);
         
         //implement mux
-        bool mux(vector<int> data,vector<int> sel); 
+
+        bool mux(vector<bool> data , vector<bool> sel); 
+        
+        //implement demux
+
+        vector<bool> demux(bool data , vector<bool> sel); 
+
+        //implement encoder
+ 
+        vector<bool> encoder(vector<bool> data);
+
+        //implement decoder
+
+        vector<bool> decoder(vector<bool> sel);
 };
+
 
 inline void Combinational::half_adder(int x, int y, int* sum, int* carry){
     *sum = xor_gate(2,x,y);
@@ -30,10 +44,54 @@ inline void Combinational::full_adder(int x, int y, int cin, int* sum, int* carr
     
     *carry = or_gate(3,wire1,wire2,wire3);
 }
-inline bool Combinational::mux(vector<int> data,vector<int> sel){
-    int ind=0;
-    for(int i=0;i<sel.size();i++){
-        ind=ind*2+sel[i];
+
+inline bool Combinational::mux(vector<bool> data,vector<bool> sel){
+    int ind = 0;
+    for(int i = 0 ; i < sel.size() ; i++){
+        ind = ind * 2 + sel[i];
     }
     return data[ind];
+}
+
+inline vector<bool> Combinational::demux(bool data , vector<bool> sel){
+    int ind = 0;
+    for(int i = 0 ; i < sel.size() ; i++){
+        ind = ind * 2 + sel[i];
+    }
+    int n = pow(2,sel.size());
+    vector<bool> out(n,false);
+    out[ind] = data;
+    return out;
+}
+inline vector<bool> Combinational::encoder(vector<bool> data){
+    int ind;
+    for(int i = 0 ; i < data.size() ; i++){
+        if(data[i] == true){
+            ind = i;
+            break;
+        }
+    }
+    int num = data.size() - ind - 1;
+
+    // finding the select lines
+    
+    int n = log2(data.size());
+    vector<bool> sel(n,0);
+    int i = n - 1;
+     while(num!=0){
+        sel[i] = num%2;
+        num = num/2;
+        i--;
+    }
+    return sel;
+}
+inline vector<bool> Combinational::decoder(vector<bool> sel){
+    int ind = 0;
+    for(int i = 0 ; i < sel.size() ; i++){
+        ind = ind * 2 + sel[i];
+    }
+    int n = pow(2,sel.size());
+    vector<bool> data(n,0);
+    data[n-ind-1] = true;
+    return data;
 }
